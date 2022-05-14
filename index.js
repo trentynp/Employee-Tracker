7086a5d5e84fe5ca8c1ab3d241e4d684dcaffcab
Trentyn <trentynpowell@gmail.com>
2022-05-14 09:34:32 -0700

    viemployee

--- index.js
+++ index.js
@@ -68,4 +68,28 @@ function firstPrompt() {
       });
   }
   
+  function viewEmployee() {
+    console.log("Viewing employees\n");
+  
+    var query =
+      `SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department, r.salary, CONCAT(m.first_name, ' ', m.last_name) AS manager
+    FROM employee e
+    LEFT JOIN role r
+      ON e.role_id = r.id
+    LEFT JOIN department d
+    ON d.id = r.department_id
+    LEFT JOIN employee m
+      ON m.id = e.manager_id`
+  
+    connection.query(query, function (err, res) {
+      if (err) throw err;
+  
+      console.table(res);
+      console.log("Employees viewed!\n");
+  
+      firstPrompt();
+    });
+      console.log(query.sql);
+  }
+
   
