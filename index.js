1bbc2c7dbc0d8b944eae3cbba614994547f63375
Trentyn <trentynpowell@gmail.com>
2022-05-14 09:35:58 -0700

    department choices

--- index.js
+++ index.js
@@ -123,3 +123,39 @@ function firstPrompt() {
     });
       console.log(query.sql);
   }
+
+  function promptDepartment(departmentChoices) {
+
+    inquirer
+      .prompt([
+        {
+          type: "list",
+          name: "departmentId",
+          message: "Which department would you choose?",
+          choices: departmentChoices
+        }
+      ])
+      .then(function (answer) {
+        console.log("answer ", answer.departmentId);
+  
+        var query =
+          `SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department 
+    FROM employee e
+    JOIN role r
+      ON e.role_id = r.id
+    JOIN department d
+    ON d.id = r.department_id
+    WHERE d.id = ?`
+  
+        connection.query(query, answer.departmentId, function (err, res) {
+          if (err) throw err;
+  
+          console.table("response ", res);
+          console.log(res.affectedRows + "Employees are viewed!\n");
+  
+          firstPrompt();
+        });
+      });
+  }
+
+  
