84347abb6b9f90d60cec4632324a11c6b62b53e9
Trentyn <trentynpowell@gmail.com>
2022-05-14 09:38:09 -0700

    delete emplyoee

--- index.js
+++ index.js
@@ -230,4 +230,51 @@ function firstPrompt() {
       });
   }
 
-
+  function removeEmployees() {
+    console.log("Deleting an employee");
+  
+    var query =
+      `SELECT e.id, e.first_name, e.last_name
+        FROM employee e`
+  
+    connection.query(query, function (err, res) {
+      if (err) throw err;
+  
+      const deleteEmployeeChoices = res.map(({ id, first_name, last_name }) => ({
+        value: id, name: `${id} ${first_name} ${last_name}`
+      }));
+  
+      console.table(res);
+      console.log("ArrayToDelete!\n");
+  
+      promptDelete(deleteEmployeeChoices);
+    });
+  }
+  
+  
+  function promptDelete(deleteEmployeeChoices) {
+  
+    inquirer
+      .prompt([
+        {
+          type: "list",
+          name: "employeeId",
+          message: "Which employee do you want to remove?",
+          choices: deleteEmployeeChoices
+        }
+      ])
+      .then(function (answer) {
+  
+        var query = `DELETE FROM employee WHERE ?`;
+       
+        connection.query(query, { id: answer.employeeId }, function (err, res) {
+          if (err) throw err;
+  
+          console.table(res);
+          console.log(res.affectedRows + "Deleted!\n");
+  
+          firstPrompt();
+        });
+         console.log(query.sql);
+      });
+  }
