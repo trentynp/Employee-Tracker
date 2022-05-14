59868e68471d8f93ad88d9dd340e83ebc464e6f7
Trentyn <trentynpowell@gmail.com>
2022-05-14 09:37:28 -0700

    EMPLOYEE ARRAY

--- index.js
+++ index.js
@@ -158,4 +158,76 @@ function firstPrompt() {
       });
   }
 
+  function addEmployee() {
+    console.log("Inserting an employee!")
   
+    var query =
+      `SELECT r.id, r.title, r.salary 
+        FROM role r`
+  
+    connection.query(query, function (err, res) {
+      if (err) throw err;
+  
+      const roleChoices = res.map(({ id, title, salary }) => ({
+        value: id, title: `${title}`, salary: `${salary}`
+      }));
+  
+      console.table(res);
+      console.log("RoleToInsert!");
+  
+      promptInsert(roleChoices);
+    });
+  }
+  
+  function promptInsert(roleChoices) {
+  
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "first_name",
+          message: "What is the employee's first name?"
+        },
+        {
+          type: "input",
+          name: "last_name",
+          message: "What is the employee's last name?"
+        },
+        {
+          type: "list",
+          name: "roleId",
+          message: "What is the employee's role?",
+          choices: roleChoices
+        },
+        {
+          name: "manager_id",
+          type: "list",
+          message: "What is the employee's manager_id?",
+          choices: manager
+        }
+      ])
+      .then(function (answer) {
+        console.log(answer);
+  
+        var query = `INSERT INTO employee SET ?`
+        
+        connection.query(query,
+          {
+            first_name: answer.first_name,
+            last_name: answer.last_name,
+            role_id: answer.roleId,
+            manager_id: answer.managerId,
+          },
+          function (err, res) {
+            if (err) throw err;
+  
+            console.table(res);
+            console.log(res.insertedRows + "Inserted successfully!\n");
+  
+            firstPrompt();
+          });
+          console.log(query.sql);
+      });
+  }
+
+
