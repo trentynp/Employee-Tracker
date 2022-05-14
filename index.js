73b697c5e731f1a8808918963c848e7bc6321b4d
Trentyn <trentynpowell@gmail.com>
2022-05-14 13:46:15 -0700

    fixes

--- index.js
+++ index.js
@@ -193,12 +193,7 @@ function firstPrompt() {
           message: "What is the employee's role?",
           choices: roleChoices
         },
-        {
-          name: "manager_id",
-          type: "list",
-          message: "What is the employee's manager_id?",
-          choices: manager
-        }
+    
       ])
       .then(function (answer) {
         console.log(answer);
