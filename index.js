ddaeb0c5322754b5629dd4037d30f0cf36600188
Trentyn <trentynpowell@gmail.com>
2022-05-14 09:41:56 -0700

    adjustments

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 require("console.table");
-// const sql = require("./sql");
+const sql = require("./sql");
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -31,7 +31,6 @@ function firstPrompt() {
           "Add Employee",
           "Remove Employees",
           "Update Employee Role",
-          "Remove Role"
           "Add Role",
           "End"]
       })
@@ -107,10 +106,10 @@ function firstPrompt() {
     connection.query(query, function (err, res) {
       if (err) throw err;
   
-     const departmentChoices = res.map(({ id, name }) => ({
-     name: `${id} ${name}`,
-     value: id
-      }));
+     //const departmentChoices = res.map(({ id, name }) => ({
+     //name: `${id} ${name}`,
+     //value: id
+     //}));
   
       const departmentChoices = res.map(data => ({
         value: data.id, name: data.name
@@ -369,3 +368,72 @@ function firstPrompt() {
          console.log(query.sql);
       });
   }
+
+  function addRole() {
+
+    var query =
+      `SELECT d.id, d.name, r.salary AS budget
+      FROM employee e
+      JOIN role r
+      ON e.role_id = r.id
+      JOIN department d
+      ON d.id = r.department_id
+      GROUP BY d.id, d.name`
+  
+    connection.query(query, function (err, res) {
+      if (err) throw err;
+  
+       // (callbackfn: (value: T, index: number, array: readonly T[]) => U, thisArg?: any)
+      const departmentChoices = res.map(({ id, name }) => ({
+        value: id, name: `${id} ${name}`
+      }));
+  
+      console.table(res);
+      console.log("Department array!");
+  
+      promptAddRole(departmentChoices);
+    });
+  }
+  
+  function promptAddRole(departmentChoices) {
+  
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "roleTitle",
+          message: "Role title?"
+        },
+        {
+          type: "input",
+          name: "roleSalary",
+          message: "Role Salary"
+        },
+        {
+          type: "list",
+          name: "departmentId",
+          message: "Department?",
+          choices: departmentChoices
+        },
+      ])
+      .then(function (answer) {
+  
+        var query = `INSERT INTO role SET ?`
+  
+        connection.query(query, {
+          title: answer.title,
+          salary: answer.salary,
+          department_id: answer.departmentId
+        },
+          function (err, res) {
+            if (err) throw err;
+  
+            console.table(res);
+            console.log("Role Inserted!");
+  
+            firstPrompt();
+          });
+  
+      });
+  }
+  
